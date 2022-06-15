b4f7a8fedfba5a46d65b59a909461efbc7e327b7
Adrian Pramana Wijaya <106787436+Adrian-Pramana-Wijaya@users.noreply.github.com>
2022-06-15 20:35:06 +0700

    Add files via upload

---
+++ mainclass/src/mainclass/Jawaban.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainclass;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Jawaban extends Pertanyaan {
+    @Override
+    void Harga(){
+       super.Harga();
+        System.out.println("Harga pisang ini 10.000");
+    }
+    @Override
+    void ApakahSegar(){
+        super.ApakahSegar();
+        System.out.println("Yaa, Pisang ini Segar");
+    }
+
+   
+    }
+    
+
---
+++ mainclass/src/mainclass/Pertanyaan.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainclass;
+
+/**
+ *
+ * @author ASUS
+ */
+class Pertanyaan {
+
+    void Harga() {
+        System.out.println("Berapa Harga Pisang ini ?");
+    }
+
+    void ApakahSegar() {
+         System.out.println("Apakah Pisang ini segar ?");
+    }
+    
+}
